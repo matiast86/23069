58cf46aa079a59a3e7904a89511fedd8b2edeffd
Matias Tailler <matiast86@gmail.com>
2023-05-07 11:23:35 -0300

    Modificaciones

---
+++ js/sumador.js
@@ -0,0 +1,15 @@
+// alert('hola mundo!!!');
+//esto es un comentario!!! de una sola linea
+
+/*
+ es 
+ un comentario
+ de 
+ varias 
+ lineas
+*/
+
+const valor1 = parseInt(prompt('Ingrese valor1'));
+const valor2 = 20;
+const resultado = valor1 + valor2;
+console.log(resultado);
